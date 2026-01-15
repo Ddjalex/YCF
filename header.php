ccc6862f30a5af9c752e73ab9ed2ccd24ad16d23
alex3510 <53130925-alex3510@users.noreply.replit.com>
2026-01-15 05:52:13 +0000

    Restored to '73bc65a290fa0817e1ac88869e5bf619e6a5f45c'
    Replit-Restored-To: 73bc65a290fa0817e1ac88869e5bf619e6a5f45c

--- header.php
+++ header.php
@@ -391,7 +391,9 @@
             <div class="nav-item">
                 <a href="/contact">Contact</a>
             </div>
-            <a href="registration.php" class="btn-custom-animate" style="font-size: 0.85rem; padding: 10px 25px;">Register Now</a>
+            <div class="header-tools" style="display: none;">
+                <span class="lang-switch btn-custom-animate">EN</span>
+            </div>
         </nav>
     </header>
     <script>
